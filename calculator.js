c264c1f12dc6a22bff75ff2a65b6f5e47cf4220d
Bruno <bdelafontaine92@gmail.com>
2022-02-25 08:48:13 -0500

    boiler plate code for post and get method

---
+++ calculator.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const bodyParser =require("body-parser");
+const app = express()
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get("/", function (req,res)
+{
+  res.sendFile(__dirname + "/index.html");
+});
+
+
+app.post("/", function (req,res){
+  var num1 = Number(req.body.n1)
+  var num2 = Number(req.body.n2)
+  var result = num1 + num2
+  // console.log(res)
+  res.send("The result is : " + result)
+});
+
+app.listen(3000, function () {
+  console.log("server is running on port 3000");
+});
